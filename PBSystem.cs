02c24a4cfb5c0f0c4f27578b00dbfbf53c666290
Sky-Meteor <2746501855@qq.com>
2022-12-19 11:55:01 +0800

    GOOOOOOOOOOOD

---
+++ PBSystem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace PlatformBuilder
+{
+    public class PBSystem : ModSystem
+    {
+        public override void AddRecipeGroups()
+        {
+            RecipeGroup dungeonbrick = new RecipeGroup(() => Language.GetTextValue("LegacyMisc.37") + "地牢砖", new int[]
+            {
+                ItemID.BlueBrick,
+                ItemID.GreenBrick,
+                ItemID.PinkBrick
+            });
+            RecipeGroup.RegisterGroup("AnyDungeonBrick", dungeonbrick);
+        }
+    }
+}
